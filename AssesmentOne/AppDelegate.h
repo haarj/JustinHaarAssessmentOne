4cd60114a0034943b3fdda2216676d5f046aece9
Justin Haar <justin.haar22@gmail.com>
2015-03-13 09:13:07 -0700

    Initial Commit

---
+++ AssesmentOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssesmentOne
+//
+//  Created by Justin Haar on 3/13/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
